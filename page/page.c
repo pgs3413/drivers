ab55b58a0a1a52737baf88888c603ec965d163d6
pan <2113114379@qq.com>
2024-10-30 23:28:17 +0800

    vmalloc

--- page/page.c
+++ page/page.c
@@ -2,31 +2,74 @@
 #include<linux/init.h>
 #include<linux/gfp.h>
 #include<linux/mm.h>
+#include<linux/vmalloc.h>
+
+// int page_init(void)
+// {
+//     struct page *page;
+//     unsigned long index, physical_addr, virtual_addr;
+
+//     page = alloc_pages(GFP_KERNEL, 0);
+
+//     printk(KERN_ALERT"page kernel virtual addr: %px\n", page_address(page));
+
+//     // page_address <=> __va(PFN_PHYS(page_to_pfn(x)))
+
+//     index = page - vmemmap; //page_to_pfn(page)
+//     physical_addr = index << PAGE_SHIFT; // PFN_PHYS(index)
+//     virtual_addr = physical_addr + PAGE_OFFSET; // __va(physical_addr)
+
+//     printk(KERN_ALERT"index: %ld physical_addr: %lx PAGE_OFFSET: %lx virtual_addr: %lx\n",
+//         index, physical_addr, PAGE_OFFSET, virtual_addr);
+
+//     __free_pages(page, 0);
+//     return 0;
+// }
+
+char *buf = 0;
+char *vbuf = 0;
+
+void print(void)
+{
+    printk(KERN_ALERT"%s\n", buf);
+    printk(KERN_ALERT"%s\n", vbuf);
+}
 
 int page_init(void)
 {
-    struct page *page;
-    unsigned long index, physical_addr, virtual_addr;
+    int size;
 
-    page = alloc_pages(GFP_KERNEL, 0);
+    buf = (char *)__get_free_pages(GFP_KERNEL, 0);
+    if(!buf)
+    {
+        printk(KERN_ALERT"get free pages fail.\n");
+        return -EFAULT;
+    }
 
-    printk(KERN_ALERT"page kernel virtual addr: %px\n", page_address(page));
+    vbuf = vmalloc(PAGE_SIZE * 10);
+    if(!vbuf)
+    {
+        printk(KERN_ALERT"vmalloc fail.\n");
+        free_pages((unsigned long)buf, 0);
+        return -EFAULT;
+    }
 
-    // page_address <=> __va(PFN_PHYS(page_to_pfn(x)))
+    printk(KERN_ALERT"module addr: %px page addr: %px vaddr: %px\n", print, buf, vbuf);
 
-    index = page - vmemmap; //page_to_pfn(page)
-    physical_addr = index << PAGE_SHIFT; // PFN_PHYS(index)
-    virtual_addr = physical_addr + PAGE_OFFSET; // __va(physical_addr)
+    size = sprintf(buf, "HELLO WORLD FORM PAGE!");
+    buf[size] = '\0';
 
-    printk(KERN_ALERT"index: %ld physical_addr: %lx PAGE_OFFSET: %lx virtual_addr: %lx\n",
-        index, physical_addr, PAGE_OFFSET, virtual_addr);
+    size = sprintf(vbuf, "HELLO WORLD FROM VMALLOC!");
+    vbuf[size] = '\0';
 
-    __free_pages(page, 0);
     return 0;
 }
 
 void page_exit(void)
 {
+    print();
+    free_pages((unsigned long)buf, 0);
+    vfree(vbuf);
 }
 
 module_init(page_init);
